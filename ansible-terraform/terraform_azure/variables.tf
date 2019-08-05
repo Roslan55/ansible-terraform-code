039d9ff78f6d1f9dbf84172980423d647e001e7a
Roslan Hydrees <roslanilan@gmail.com>
2019-08-05 23:11:00 +0100

    Change host name test

--- ansible-terraform/terraform_azure/variables.tf
+++ ansible-terraform/terraform_azure/variables.tf
@@ -29,7 +29,7 @@ variable "prefix" {
 
 variable "hostname" {
   description = "Virtual machine hostname. Used for local hostname, DNS, and storage-related names."
-  default     = "catapp"
+  default     = "catapptest"
 }
 
 variable "location" {
